fc6c1ae2ba267909f4a7778e77b7563a8dc732aa
kris <krishnakumars0101@gmail.com>
2025-05-18 00:31:17 +0530

    basic data types

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+// const msg:string="Hello"
+// msg()
+var naame = "Kris";
+var numar = [1, 2, 3, 4, 5, 5,];
+var names = ["k", "r", "i", "s", "kris"];
+var unknwndata = "42";
---
+++ app.ts
@@ -0,0 +1,9 @@
+console.log("Hello World");
+// const msg:string="Hello"
+// msg()
+
+let naame: string = "Kris";
+let numar:number[]=[1,2,3,4,5,5,]
+let names : Array<string> = ["k","r","i","s","kris"]
+
+let unknwndata:any="42";
