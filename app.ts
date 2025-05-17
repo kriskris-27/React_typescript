6a6112d0a01648f21331edfa4f08cbaafbdf3458
kris <krishnakumars0101@gmail.com>
2025-05-18 00:52:41 +0530

    functions covered

--- app.ts
+++ app.ts
@@ -2,8 +2,30 @@ console.log("Hello World");
 // const msg:string="Hello"
 // msg()
 
-let naame: string = "Kris";
-let numar:number[]=[1,2,3,4,5,5,]
-let names : Array<string> = ["k","r","i","s","kris"]
+// let naame: string = "Kris";
+// let numar:number[]=[1,2,3,4,5,5,]
+// let names : Array<string> = ["k","r","i","s","kris"]
 
-let unknwndata:any="42";
+// let unknwndata:any="42";
+
+//functions
+// function greetUser(name:string){
+//     console.log("hi " + name);
+// }
+
+// greetUser("kris")
+
+// function numres(mynum:number):number{
+//     console.log(mynum);
+//     return mynum;
+// }
+
+// numres(42);
+
+async function getdata():Promise<string>{
+    const response: Response = await fetch("https:///example.com")
+    console.log("data received");
+    
+    return "data received"
+}
+getdata();
